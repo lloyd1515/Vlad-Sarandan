e9f0280fe8e0b8bf679377d2ccf508319c54cc53
Vlad Sarandan <lloyd15150@gmail.com>
2026-08-10 23:40:37 +0300

    feat: complete app integration, localstorage persistence and css styling

--- react-assignment/src/components/PokemonFilters/PokemonFilters.tsx
+++ react-assignment/src/components/PokemonFilters/PokemonFilters.tsx
@@ -1,47 +1,51 @@
-    import React from 'react';                                                                                   
-    import type { CapturedFilter } from '../../types/pokemon';
-  
-    // 1. Ce date primește filtrul de la părinte
-    interface PokemonFiltersProps {
-      types: string[];
-      selectedType: string;
-      selectedCaptured: CapturedFilter;
-      onTypeChange: (type: string) => void;
-      onCapturedChange: (filter: CapturedFilter) => void;
-    }
-  
-    export const PokemonFilters: React.FC<PokemonFiltersProps> = ({
-      types,
-      selectedType,
-      selectedCaptured,
-      onTypeChange,
-      onCapturedChange,
-    }) => {
-      return (
-        <div className="filters-container">
-          {/* Dropdown 1: Filtru după Tip (Fire, Water, Grass...) */}
-          <div className="filter-group">
-            <label>Type</label>
-            <select value={selectedType} onChange={(e) => onTypeChange(e.target.value)}>
-              <option value="Any">Any</option>
-              {types.map((t) => (
-                <option key={t} value={t}>{t}</option>
-              ))}
-            </select>
-          </div>
-  
-          {/* Dropdown 2: Filtru după Status Capturat */}
-          <div className="filter-group">
-            <label>Captured</label>
-            <select
-              value={selectedCaptured}
-              onChange={(e) => onCapturedChange(e.target.value as CapturedFilter)}
-            >
-              <option value="Any">Any</option>
-              <option value="Captured">Captured</option>
-              <option value="Not Captured">Not Captured</option>
-            </select>
-          </div>
-        </div>
-      );
-    };
+import React from 'react';
+import type { CapturedFilter } from '../../types/pokemon';
+
+interface PokemonFiltersProps {
+  types: string[];
+  selectedType: string;
+  selectedCaptured: CapturedFilter;
+  onTypeChange: (type: string) => void;
+  onCapturedChange: (filter: CapturedFilter) => void;
+}
+
+export const PokemonFilters: React.FC<PokemonFiltersProps> = ({
+  types,
+  selectedType,
+  selectedCaptured,
+  onTypeChange,
+  onCapturedChange,
+}) => {
+  return (
+    <div className="filters-bar">
+      <div className="filter-wrapper">
+        <fieldset className="filter-fieldset">
+          <legend className="filter-legend">Type</legend>
+          <select
+            value={selectedType}
+            onChange={(e) => onTypeChange(e.target.value)}
+            className="filter-select"
+          >
+            <option value="Any">Any</option>
+            {types.map((t) => (
+              <option key={t} value={t}>{t}</option>
+            ))}
+          </select>
+        </fieldset>
+
+        <fieldset className="filter-fieldset">
+          <legend className="filter-legend">Captured</legend>
+          <select
+            value={selectedCaptured}
+            onChange={(e) => onCapturedChange(e.target.value as CapturedFilter)}
+            className="filter-select"
+          >
+            <option value="Any">Any</option>
+            <option value="Captured">Captured</option>
+            <option value="Not Captured">Not Captured</option>
+          </select>
+        </fieldset>
+      </div>
+    </div>
+  );
+};
